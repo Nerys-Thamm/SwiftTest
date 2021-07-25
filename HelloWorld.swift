e7edfd7411ad82e70821d8a10becda5802bd1ad9
Nerys-Thamm <nerysthamm@gmail.com>
2021-07-26 11:06:15 +1200

    Tried some stuff

--- HelloWorld.swift
+++ HelloWorld.swift
@@ -1,1 +1,12 @@
-print("HelloWorld");
+print("Please say the magic word!");
+
+var teststr : String = readLine() ?? "";
+
+switch teststr.uppercased(){
+    case "PLEASE", "ABRACADABRA":
+        print("Huzzah!");
+    default:
+        print("YOU STUPID USER, GO AWAY!");
+    
+    
+}
